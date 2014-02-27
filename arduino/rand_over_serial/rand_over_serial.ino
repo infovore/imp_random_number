424be4e79522b63c83d81c297564cbace55daa8b
Tom Armitage <tom@infovore.org>
2014-02-27 11:42:17 +0000

    Final arduino code.

--- arduino/rand_over_serial/rand_over_serial.ino
+++ arduino/rand_over_serial/rand_over_serial.ino
@@ -3,9 +3,13 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <SerialCommand.h>
+#include <SoftwareSerial.h>
 
-SerialCommand sCmd;     // The demo SerialCommand object
+SoftwareSerial impSerial(8,7); // RX on 8, TX on 7 (we're going to ignore TX for now);
 
+SerialCommand sCmd(impSerial);     // The SerialCommand object
+
+// set up our OLED
 // If using software SPI (the default case):
 #define OLED_MOSI   9
 #define OLED_CLK   10
@@ -14,27 +18,24 @@ SerialCommand sCmd;     // The demo SerialCommand object
 #define OLED_RESET 13
 Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
-
-
 #if (SSD1306_LCDHEIGHT != 32)
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
 void setup()   {                
   Serial.begin(9600);
+  impSerial.begin(9600);
     
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC);
   display.clearDisplay();
-  
-  Serial.begin(9600);
 
   // Setup callbacks for SerialCommand commands
   sCmd.addCommand("N",     handleNumber);  // Converts two arguments to integers and echos them back
-  sCmd.setDefaultHandler(unrecognized);      // Handler for command that isn't matched  (says "What?")
+  sCmd.addDefaultHandler(unrecognized);      // Handler for command that isn't matched  (says "What?")
   Serial.println("Ready");
   
-  // text display tests
+  // text display setup
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(48,0);
@@ -47,11 +48,12 @@ void loop() {
 }
 
 void handleNumber() {
-  
   char *arg;
   arg = sCmd.next();    // Get the next argument from the SerialCommand object buffer
   if (arg != NULL)      // As long as it existed, take it
   {
+    Serial.println("Saw data");
+    Serial.println(arg);
     display.clearDisplay();
     display.setTextSize(2);
     display.setCursor(0,0);
@@ -64,6 +66,6 @@ void handleNumber() {
 
 }
 
-void unrecognized(const char *command) {
+void unrecognized() {
   Serial.println("What?");
 }
